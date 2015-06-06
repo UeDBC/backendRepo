679b5a765030fbeec266a828a84c91a2c116dd46
Francisco J. Capdevila <fjcapdevila@gmail.com>
2015-06-06 18:23:08 -0300

    Vistas API de Patologia.

--- Epi/encuestas/views.py
+++ Epi/encuestas/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 
-from encuestas.models import Encuesta, Vivienda, Pueblo
+from encuestas.models import Encuesta, Vivienda, Pueblo, Individuo, Patologia
 from encuestas.serializers import (EncuestaSerializer, ViviendaSerializer,
-                                   PuebloSerializer)
+                                   PuebloSerializer, IndividuoSerializer,
+                                   PatologiaSerializer)
 
 
 # Create your views here.
@@ -22,3 +23,13 @@ class ViviendaViewSet(viewsets.ModelViewSet):
 class PuebloViewSet(viewsets.ModelViewSet):
     queryset = Pueblo.objects.all()
     serializer_class = PuebloSerializer
+
+
+class IndividuoViewSet(viewsets.ModelViewSet):
+    queryset = Individuo.objects.all()
+    serializer_class = IndividuoSerializer
+
+
+class PatologiaViewSet(viewsets.ModelViewSet):
+    queryset = Patologia.objects.all()
+    serializer_class = PatologiaSerializer
